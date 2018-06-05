ddc6b5be8c0daa8ae2cba7260b42c54855ff149e
Georgi Dankov <gddankov@gmail.com>
2018-06-05 14:33:24 +0200

    update vendors
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- vendor/code.cloudfoundry.org/goshims/ldapshim/ldap.go
+++ vendor/code.cloudfoundry.org/goshims/ldapshim/ldap.go
@@ -1,6 +1,7 @@
 package ldapshim
 
 import (
+	"crypto/tls"
 	"time"
 
 	"gopkg.in/ldap.v2"
@@ -9,6 +10,7 @@ import (
 //go:generate counterfeiter -o ldap_fake/fake_ldap.go . Ldap
 type Ldap interface {
 	Dial(network, addr string) (LdapConnection, error)
+	DialTLS(network, addr string, config *tls.Config) (LdapConnection, error)
 	NewSearchRequest(string, int, int, int, int, bool, string, []string, []ldap.Control) *ldap.SearchRequest
 }
 
