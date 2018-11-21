4367b78d264c9f1954f43c0bc2648f71d66ed8ff
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-11-21 15:43:18 +0100

    add events package containing the crash reporter
    [#162056092]

---
+++ events/crashreporter.go
@@ -0,0 +1,41 @@
+package events
+
+import (
+	"code.cloudfoundry.org/eirini/route"
+	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/runtimeschema/cc_messages"
+)
+
+//go:generate counterfeiter . CcClient
+type CcClient interface {
+	AppCrashed(proccessGuid string, crashedRequest cc_messages.AppCrashedRequest, logger lager.Logger)
+}
+
+type CrashReport struct {
+	ProcessGuid string
+	cc_messages.AppCrashedRequest
+}
+
+type CrashReporter struct {
+	reports   <-chan CrashReport
+	scheduler route.TaskScheduler
+	client    CcClient
+	logger    lager.Logger
+}
+
+func NewCrashReporter(reportChan <-chan CrashReport, scheduler route.TaskScheduler, client CcClient, logger lager.Logger) *CrashReporter {
+	return &CrashReporter{
+		reports:   reportChan,
+		scheduler: scheduler,
+		client:    client,
+		logger:    logger,
+	}
+}
+
+func (c *CrashReporter) Run() {
+	c.scheduler.Schedule(func() error {
+		report := <-c.reports
+		c.client.AppCrashed(report.ProcessGuid, report.AppCrashedRequest, c.logger)
+		return nil
+	})
+}
