b11dc990f7fbab309c1ebc2772da416e4446442e
Akshay Mankar <amankar@pivotal.io>
2019-03-29 16:37:54 +0000

    Use backticks to avoid duble escaping
    [#164204947]

--- k8s/utils/names.go
+++ k8s/utils/names.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SanitizeName(name string, fallback string) string {
-	validNameRegex := regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$")
+	validNameRegex := regexp.MustCompile(`^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$`)
 	sanitizedName := strings.ReplaceAll(strings.ToLower(name), "_", "-")
 	if validNameRegex.MatchString(sanitizedName) {
 		return sanitizedName
