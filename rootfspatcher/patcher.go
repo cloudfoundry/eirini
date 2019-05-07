dd50135aa7d4212cc2fa1d545258fc714b820ed5
Oleksandr Slynko <oslynko@pivotal.io>
2019-05-07 12:14:23 +0100

    Use a copy of statefulset to fix linter
    [#162699664]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- rootfspatcher/patcher.go
+++ rootfspatcher/patcher.go
@@ -33,9 +33,10 @@ func (p StatefulSetPatcher) Patch() error {
 	failuresOccured := 0
 	p.Logger.Info(fmt.Sprintf("found %d stateful sets to patch", len(sts.Items)))
 	for _, s := range sts.Items {
-		s.Labels[RootfsVersionLabel] = p.Version
-		s.Spec.Template.Labels[RootfsVersionLabel] = p.Version
-		_, err := p.StatefulSets.Update(&s)
+		statesfulset := s
+		statesfulset.Labels[RootfsVersionLabel] = p.Version
+		statesfulset.Spec.Template.Labels[RootfsVersionLabel] = p.Version
+		_, err := p.StatefulSets.Update(&statesfulset)
 		if err != nil {
 			p.Logger.Error("failed to patch", err)
 			failuresOccured++
