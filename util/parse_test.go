3a53df1dbdf9ec6c389ce3a7b3a8019d9cca9951
Oleksandr Slynko <oslynko@pivotal.io>
2019-05-24 16:21:19 +0100

    Improve parse_test.go
    * Use better way to not expect errors
    http://onsi.github.io/gomega/#handling-errors
    * Ignore other return values when we expect errors
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- util/parse_test.go
+++ util/parse_test.go
@@ -9,28 +9,23 @@ import (
 
 var _ = Describe("Parse", func() {
 	It("should index from pod name", func() {
-		index, err := util.ParseAppIndex("some-name-1")
-
-		Expect(err).ToNot(HaveOccurred())
-		Expect(index).To(Equal(1))
+		Expect(util.ParseAppIndex("some-name-1")).To(Equal(1))
 	})
 
 	Context("when the pod name does not contain dashes", func() {
 		It("should return an error", func() {
-			index, err := util.ParseAppIndex("somename1")
+			_, err := util.ParseAppIndex("somename1")
 
 			Expect(err).To(HaveOccurred())
-			Expect(index).To(Equal(0))
 		})
 
 	})
 
 	Context("when the last part in pod name is not a number", func() {
 		It("should return an error", func() {
-			index, err := util.ParseAppIndex("somename-a")
+			_, err := util.ParseAppIndex("somename-a")
 
 			Expect(err).To(HaveOccurred())
-			Expect(index).To(Equal(0))
 		})
 
 	})
