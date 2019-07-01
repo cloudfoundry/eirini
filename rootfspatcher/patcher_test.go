dca4fc350f697236b53e89890cc3b88776d78859
Georgi Dankov <gddankov@gmail.com>
2019-07-01 12:50:31 +0300

    Refactor StatefulSetPatcher test
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- rootfspatcher/patcher_test.go
+++ rootfspatcher/patcher_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	v1 "k8s.io/api/apps/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -22,8 +22,7 @@ var _ = Describe("Patcher", func() {
 		patcher                  StatefulSetPatcher
 		newVersion               string
 		logger                   *lagertest.TestLogger
-		err                      error
-		stsList                  *v1.StatefulSetList
+		stsList                  *appsv1.StatefulSetList
 	)
 
 	BeforeEach(func() {
@@ -36,8 +35,8 @@ var _ = Describe("Patcher", func() {
 			Logger:       logger,
 		}
 
-		stsList = &v1.StatefulSetList{
-			Items: []v1.StatefulSet{
+		stsList = &appsv1.StatefulSetList{
+			Items: []appsv1.StatefulSet{
 				createStatefulSet("some-app", "version1"),
 			},
 		}
@@ -45,52 +44,40 @@ var _ = Describe("Patcher", func() {
 		statefulsetUpdaterLister.ListReturns(stsList, nil)
 	})
 
-	JustBeforeEach(func() {
-		err = patcher.Patch()
-	})
-
 	Context("When a new version is provided", func() {
-
 		It("should succeed", func() {
-			Expect(err).ToNot(HaveOccurred())
+			Expect(patcher.Patch()).To(Succeed())
 		})
 
 		It("should call the updated function once", func() {
+			Expect(patcher.Patch()).To(Succeed())
 			Expect(statefulsetUpdaterLister.UpdateCallCount()).To(Equal(1))
 		})
 
 		It("should update all statefulsets labels with the new version", func() {
+			Expect(patcher.Patch()).To(Succeed())
 			updatedStatefulset := statefulsetUpdaterLister.UpdateArgsForCall(0)
 			Expect(updatedStatefulset.Labels).To(HaveKeyWithValue(RootfsVersionLabel, newVersion))
 		})
 
 		It("should update all statefulsets template specs with the new version", func() {
+			Expect(patcher.Patch()).To(Succeed())
 			updatedStatefulset := statefulsetUpdaterLister.UpdateArgsForCall(0)
 			Expect(updatedStatefulset.Spec.Template.Labels).To(HaveKeyWithValue(RootfsVersionLabel, newVersion))
 		})
 
-		It("should log a line for each statefulset update", func() {
-
-		})
-
 		Context("When Patch fails", func() {
 			Context("because it cannot list statefulsets", func() {
-				BeforeEach(func() {
-					statefulsetUpdaterLister.ListReturns(&v1.StatefulSetList{}, errors.New("arrrgh"))
-				})
-
 				It("should fail with a meaningful error message", func() {
-					Expect(err).To(MatchError(ContainSubstring("failed to list statefulset")))
+					statefulsetUpdaterLister.ListReturns(&appsv1.StatefulSetList{}, errors.New("arrrgh"))
+					Expect(patcher.Patch()).To(MatchError(ContainSubstring("failed to list statefulset")))
 				})
 			})
 
 			Context("because it cannot update statefulsets", func() {
-				BeforeEach(func() {
-					statefulsetUpdaterLister.UpdateReturns(&v1.StatefulSet{}, errors.New("brrrgh"))
-				})
-
 				It("should fail with a meaningful error message", func() {
-					Expect(err).To(MatchError(ContainSubstring("failed to update 1 statefulsets")))
+					statefulsetUpdaterLister.UpdateReturns(&appsv1.StatefulSet{}, errors.New("brrrgh"))
+					Expect(patcher.Patch()).To(MatchError(ContainSubstring("failed to update 1 statefulsets")))
 				})
 			})
 		})
@@ -100,15 +87,13 @@ var _ = Describe("Patcher", func() {
 				stsList.Items = append(stsList.Items, createStatefulSet("another-app", "version2"))
 			})
 
-			It("should succeed", func() {
-				Expect(err).ToNot(HaveOccurred())
-			})
-
 			It("should call the updated function once", func() {
+				Expect(patcher.Patch()).To(Succeed())
 				Expect(statefulsetUpdaterLister.UpdateCallCount()).To(Equal(2))
 			})
 
 			It("should update all statefulsets labels with the new version", func() {
+				Expect(patcher.Patch()).To(Succeed())
 				updatedStatefulset := statefulsetUpdaterLister.UpdateArgsForCall(0)
 				updatedStatefulset2 := statefulsetUpdaterLister.UpdateArgsForCall(1)
 
@@ -117,6 +102,7 @@ var _ = Describe("Patcher", func() {
 			})
 
 			It("should update all statefulsets template specs with the new version", func() {
+				Expect(patcher.Patch()).To(Succeed())
 				updatedStatefulset := statefulsetUpdaterLister.UpdateArgsForCall(0)
 				updatedStatefulset2 := statefulsetUpdaterLister.UpdateArgsForCall(1)
 
@@ -125,25 +111,22 @@ var _ = Describe("Patcher", func() {
 			})
 
 			Context("When Patch fails with multiple statefulsets", func() {
-				BeforeEach(func() {
-					statefulsetUpdaterLister.UpdateReturns(&v1.StatefulSet{}, errors.New("brrrgh"))
-				})
-
 				It("should fail with a meaningful error message", func() {
-					Expect(err).To(MatchError(ContainSubstring("failed to update 2 statefulsets")))
+					statefulsetUpdaterLister.UpdateReturns(&appsv1.StatefulSet{}, errors.New("brrrgh"))
+					Expect(patcher.Patch()).To(MatchError(ContainSubstring("failed to update 2 statefulsets")))
 				})
 			})
 		})
 	})
 })
 
-func createStatefulSet(name, version string) v1.StatefulSet {
-	return v1.StatefulSet{
+func createStatefulSet(name, version string) appsv1.StatefulSet {
+	return appsv1.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: map[string]string{RootfsVersionLabel: version},
 		},
-		Spec: v1.StatefulSetSpec{
+		Spec: appsv1.StatefulSetSpec{
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{RootfsVersionLabel: version},
