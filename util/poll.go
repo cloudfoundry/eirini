19a065913a57fca5520f68872461d60ee46f4730
Akshay Mankar <amankar@pivotal.io>
2019-08-28 11:08:07 +0100

    Extract `PollUntilTrue` from bits-waiter
    [#168072074]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

---
+++ util/poll.go
@@ -0,0 +1,19 @@
+package util
+
+import "time"
+
+type PollFunc func() bool
+
+func PollUntilTrue(f PollFunc, d time.Duration, stop <-chan interface{}) bool {
+	ticker := time.NewTicker(d)
+	for {
+		select {
+		case <-stop:
+			return false
+		case <-ticker.C:
+			if f() {
+				return true
+			}
+		}
+	}
+}
