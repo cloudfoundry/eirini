fd6adbde1c110d6dc6b09499e6039b982c88a7b0
Oleksandr Slynko <oslynko@pivotal.io>
2019-10-16 16:09:58 +0100

    Trim path from binaries

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ readonly BASEDIR="$(cd "$(dirname "$0")"/.. && pwd)"
 
 export GO111MODULE=on
 pushd "$BASEDIR" > /dev/null || exit 1
-  go build -mod vendor -o /dev/null ./cmd/opi
-  go build -mod vendor -o /dev/null ./cmd/rootfs-patcher
-  go build -mod vendor -o /dev/null ./cmd/bits-waiter
+  go build -trimpath -mod vendor -o /dev/null ./cmd/opi
+  go build -trimpath -mod vendor -o /dev/null ./cmd/rootfs-patcher
+  go build -trimpath -mod vendor -o /dev/null ./cmd/bits-waiter
 popd > /dev/null || exit 1
