941a80e10bd0b3344d6ecbefc0f9cb0e61e3b8e1
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-07-29 15:21:19 +0000

    Increase the default eventually timeout for the task-reporter suite
    Signed-off-by: Danail Branekov <danailster@gmail.com>

--- integration/task-reporter/task_reporter_suite_test.go
+++ integration/task-reporter/task_reporter_suite_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestStagingReporter(t *testing.T) {
-	SetDefaultEventuallyTimeout(60 * time.Second)
+	SetDefaultEventuallyTimeout(2 * time.Minute)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "TaskReporter Suite")
 }
