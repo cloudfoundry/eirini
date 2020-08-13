f73e576ef17779f1c6572a36b5ba40fa54e4b935
Georgi Sabev <georgethebeatle@gmail.com>
2020-08-13 13:07:50 +0000

    Script for running eats tests only
    [#173809746]
    
    Signed-off-by: Danail Branekov <danailster@gmail.com>

---
+++ scripts/run_eats_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly BASEDIR="$(cd "$(dirname "$0")"/.. && pwd)"
+export GO111MODULE=on
+export EIRINIUSER_PASSWORD="${EIRINIUSER_PASSWORD:-$(pass eirini/docker-hub)}"
+
+main() {
+  pushd "$BASEDIR"/integration/eats >/dev/null || exit 1
+  ginkgo -mod=vendor -p -r -keepGoing -randomizeAllSpecs -randomizeSuites -timeout=20m "$@"
+  popd >/dev/null || exit 1
+}
+
+main "$@"
