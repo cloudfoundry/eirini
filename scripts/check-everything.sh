c85e70a7a2a199d563d64e251b9342d0a64b3d47
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-08-25 09:54:41 +0000

    Run integration tests locally against kind
    we do this to guarantee that integration tests are run against an
    empty cluster
    
    [#174120957]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- scripts/check-everything.sh
+++ scripts/check-everything.sh
@@ -8,7 +8,16 @@ RUN_DIR="$(cd "$(dirname "$0")" && pwd)"
 export GO111MODULE=on
 "$RUN_DIR"/run_unit_tests.sh
 "$RUN_DIR"/build.sh
-"$RUN_DIR"/run_integration_tests.sh
+
+readonly kubeconfig=$(mktemp)
+trap "rm $kubeconfig" EXIT
+if ! kind get clusters | grep -q integration-tests; then
+  current_cluster="$(kubectl config current-context)"
+  kind create cluster --name integration-tests
+  kubectl config use-context "$current_cluster"
+fi
+kind get kubeconfig --name integration-tests >$kubeconfig
+INTEGRATION_KUBECONFIG=$kubeconfig "$RUN_DIR"/run_integration_tests.sh
 
 echo "Running Linter"
 cd "$RUN_DIR"/.. || exit 1
