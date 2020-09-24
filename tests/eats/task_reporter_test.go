689c415a66053540ed5e8e4b13f77b3815603c13
Danail Branekov <danailster@gmail.com>
2020-09-24 15:32:01 +0100

    Use wiremock in task reporter EATS
    Checks CC gets called correctly and that job remains when call fails
    
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

--- tests/eats/task_reporter_test.go
+++ tests/eats/task_reporter_test.go
@@ -9,27 +9,32 @@ import (
 	"code.cloudfoundry.org/eirini/k8s"
 	"code.cloudfoundry.org/eirini/models/cf"
 	"code.cloudfoundry.org/eirini/tests"
+	"code.cloudfoundry.org/eirini/tests/eats/wiremock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-// Deliberately pend this test as the task reporter would now not delete
-// the job if notifying the cloud controller fails. We need to implement
-// some sort of callback verification facility (wiremock maybe) that does
-// handles the invocation callback
 var _ = Describe("Tasks Reporter", func() {
-	var taskRequest cf.TaskRequest
+	var (
+		taskRequest        cf.TaskRequest
+		taskGUID           string
+		callbackStatusCode int
+	)
 
 	BeforeEach(func() {
+
+		taskGUID = tests.GenerateGUID()
+		callbackStatusCode = http.StatusOK
+
 		taskRequest = cf.TaskRequest{
-			GUID:               tests.GenerateGUID(),
+			GUID:               taskGUID,
 			Namespace:          fixture.Namespace,
 			Name:               "some-task",
 			AppGUID:            tests.GenerateGUID(),
 			AppName:            "some-app",
 			OrgName:            "the-org",
 			SpaceName:          "the-space",
-			CompletionCallback: "http://example.com/",
+			CompletionCallback: fmt.Sprintf("%s/%s", fixture.Wiremock.URL, taskGUID),
 			Lifecycle: cf.Lifecycle{
 				DockerLifecycle: &cf.DockerLifecycle{
 					Image: "busybox",
@@ -43,12 +48,48 @@ var _ = Describe("Tasks Reporter", func() {
 	})
 
 	JustBeforeEach(func() {
+		err := fixture.Wiremock.AddStub(wiremock.Stub{
+			Request: wiremock.RequestMatcher{
+				Method: "POST",
+				URL:    fmt.Sprintf("/%s", taskGUID),
+			},
+			Response: wiremock.Response{
+				Status: callbackStatusCode,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+
 		desireOpiTask(taskRequest)
 		Eventually(jobExists(taskRequest.GUID)).Should(BeTrue())
 	})
 
 	It("deletes the task after it completes", func() {
 		Eventually(jobExists(taskRequest.GUID)).Should(BeFalse())
+	})
+
+	It("notifies the cloud controller", func() {
+		requestMatcher := wiremock.RequestMatcher{
+			Method: "POST",
+			URL:    fmt.Sprintf("/%s", taskGUID),
+		}
+		Eventually(fixture.Wiremock.GetCountFn(requestMatcher), "1m").Should(Equal(1))
+	})
+
+	When("posting to the cloud controller fails", func() {
+		BeforeEach(func() {
+			callbackStatusCode = http.StatusTeapot
+		})
+
+		It("does not delete the job", func() {
+			requestMatcher := wiremock.RequestMatcher{
+				Method: "POST",
+				URL:    fmt.Sprintf("/%s", taskGUID),
+			}
+			Eventually(fixture.Wiremock.GetCountFn(requestMatcher), "1m").Should(Equal(1))
+
+			Expect(jobExists(taskRequest.GUID)()).To(BeTrue())
+		})
+
 	})
 })
 
