401f8d1af16c17f6bc3786809c39998f67097877
Danail Branekov <danailster@gmail.com>
2020-10-27 17:24:10 +0200

    Introduce `exit` endpoint into notdora
    [#174817357]
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tests/integration/assets/notdora/main.go
+++ tests/integration/assets/notdora/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -12,7 +13,8 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/exit", ExitHandler)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("an unexpected error occurred: %w", err))
@@ -20,6 +22,28 @@ func main() {
 	}
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hi, I'm not Dora!")
+}
+
+func ExitHandler(w http.ResponseWriter, r *http.Request) {
+	exitCodes, ok := r.URL.Query()["exitCode"]
+	if !ok {
+		os.Exit(0)
+	}
+
+	if len(exitCodes) != 1 {
+		fmt.Fprintf(w, "one exit code value expected, found %d in %v", len(exitCodes), exitCodes)
+
+		return
+	}
+
+	exitCode, err := strconv.Atoi(exitCodes[0])
+	if err != nil {
+		fmt.Fprintf(w, "invalid exit code value: %s", exitCodes)
+
+		return
+	}
+
+	os.Exit(exitCode)
 }
