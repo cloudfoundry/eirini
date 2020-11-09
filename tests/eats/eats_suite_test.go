dd6bce28ea6742ad6e11413580811f29caceded0
Georgi Dankov <gddankov@gmail.com>
2020-11-09 14:45:53 +0000

    Bump timeout even more (see previous commit)
    [#175611453]
    
    Co-authored-by: Georgi Dankov <gddankov@gmail.com>
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tests/eats/eats_suite_test.go
+++ tests/eats/eats_suite_test.go
@@ -22,7 +22,7 @@ import (
 )
 
 func TestEats(t *testing.T) {
-	SetDefaultEventuallyTimeout(5 * time.Minute)
+	SetDefaultEventuallyTimeout(10 * time.Minute)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Eats Suite")
 }
