4879923f28a7c71094b9da1537ee1961dd5c78b5
Kieron Browne <kbrowne@vmware.com>
2020-11-17 18:59:01 +0200

    Revert events reporter test from 3cac3046
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- tests/eats/events_reporter_test.go
+++ tests/eats/events_reporter_test.go
@@ -1,22 +1,28 @@
 package eats_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 
+	"code.cloudfoundry.org/eirini/k8s"
 	"code.cloudfoundry.org/eirini/models/cf"
 	"code.cloudfoundry.org/eirini/tests"
 	"code.cloudfoundry.org/eirini/tests/eats/wiremock"
 	"code.cloudfoundry.org/runtimeschema/cc_messages"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("EventsReporter", func() {
 	var (
 		guid            string
 		version         string
+		appServiceName  string
 		expectedRequest wiremock.RequestMatcher
 	)
 
@@ -59,67 +65,52 @@ var _ = Describe("EventsReporter", func() {
 				},
 			})
 			Expect(statusCode).To(Equal(http.StatusAccepted))
+
+			appServiceName = exposeLRP(fixture.Namespace, guid, 8080, "/")
+		})
+
+		AfterEach(func() {
+			unexposeLRP(fixture.Namespace, appServiceName)
 		})
 
 		It("does not report a crash event for running apps", func() {
 			Consistently(fixture.Wiremock.GetCountFn(expectedRequest), "10s").Should(BeZero())
 		})
-	})
 
-	When("the app exits with non-zero code", func() {
-		BeforeEach(func() {
-			statusCode := desireLRP(cf.DesireLRPRequest{
-				Namespace:    fixture.Namespace,
-				GUID:         guid,
-				Version:      version,
-				NumInstances: 1,
-				DiskMB:       512,
-				Lifecycle: cf.Lifecycle{
-					DockerLifecycle: &cf.DockerLifecycle{
-						Image:            "eirini/busybox",
-						Command:          []string{"/bin/sh", "-c", "exit 1"},
-						RegistryUsername: "eiriniuser",
-						RegistryPassword: tests.GetEiriniDockerHubPassword(),
-					},
-				},
+		When("the app exits with non-zero code", func() {
+			BeforeEach(func() {
+				_, err := http.Get(fmt.Sprintf("http://%s.%s:8080/exit?exitCode=1", appServiceName, fixture.Namespace))
+				Expect(err).To(MatchError(ContainSubstring("EOF"))) // The app exited
 			})
-			Expect(statusCode).To(Equal(http.StatusAccepted))
-		})
 
-		It("reports a crash event", func() {
-			Eventually(fixture.Wiremock.GetCountFn(expectedRequest)).Should(BeNumerically(">=", 1))
-			verifyCrashRequest(expectedRequest, 1)
-		})
-	})
+			It("reports a crash event", func() {
+				Eventually(fixture.Wiremock.GetCountFn(expectedRequest)).Should(Equal(1))
+				Consistently(fixture.Wiremock.GetCountFn(expectedRequest), "10s").Should(Equal(1))
 
-	When("the app exits with zero code", func() {
-		BeforeEach(func() {
-			statusCode := desireLRP(cf.DesireLRPRequest{
-				Namespace:    fixture.Namespace,
-				GUID:         guid,
-				Version:      version,
-				NumInstances: 1,
-				DiskMB:       512,
-				Lifecycle: cf.Lifecycle{
-					DockerLifecycle: &cf.DockerLifecycle{
-						Image:            "eirini/busybox",
-						Command:          []string{"/bin/sh", "-c", "exit 0"},
-						RegistryUsername: "eiriniuser",
-						RegistryPassword: tests.GetEiriniDockerHubPassword(),
-					},
-				},
+				verifyCrashRequest(expectedRequest, 1)
 			})
-			Expect(statusCode).To(Equal(http.StatusAccepted))
 		})
 
-		It("reports a crash event", func() {
-			Eventually(fixture.Wiremock.GetCountFn(expectedRequest)).Should(BeNumerically(">=", 1))
-			verifyCrashRequest(expectedRequest, 0)
+		When("the app exits with zero code", func() {
+			BeforeEach(func() {
+				_, err := http.Get(fmt.Sprintf("http://%s.%s:8080/exit?exitCode=0", appServiceName, fixture.Namespace))
+				Expect(err).To(MatchError(ContainSubstring("EOF"))) // The app exited
+			})
+
+			It("reports a crash event", func() {
+				Eventually(fixture.Wiremock.GetCountFn(expectedRequest)).Should(Equal(1))
+				Consistently(fixture.Wiremock.GetCountFn(expectedRequest), "10s").Should(Equal(1))
+
+				verifyCrashRequest(expectedRequest, 0)
+			})
 		})
 	})
 
 	When("the app is crashing on startup", func() {
 		BeforeEach(func() {
+			_, err := stopLRP(guid, version)
+			Expect(err).NotTo(HaveOccurred())
+
 			statusCode := desireLRP(cf.DesireLRPRequest{
 				Namespace:    fixture.Namespace,
 				GUID:         guid,
@@ -144,6 +135,53 @@ var _ = Describe("EventsReporter", func() {
 	})
 })
 
+func exposeLRP(namespace, guid string, appPort int32, pingPath ...string) string {
+	service, err := fixture.Clientset.CoreV1().Services(namespace).Create(context.Background(), &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "service-" + guid,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port: appPort,
+				},
+			},
+			Selector: map[string]string{
+				k8s.LabelGUID: guid,
+			},
+		},
+	}, metav1.CreateOptions{})
+	Expect(err).NotTo(HaveOccurred())
+
+	if len(pingPath) > 0 {
+		pingURL := &url.URL{
+			Scheme: "http",
+			Host:   fmt.Sprintf("%s.%s:%d", service.Name, namespace, appPort),
+			Path:   pingPath[0],
+		}
+
+		Eventually(func() error {
+			resp, err := http.Get(pingURL.String())
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("request failed: %s", resp.Status)
+			}
+
+			return nil
+		}).Should(Succeed())
+	}
+
+	return service.Name
+}
+
+func unexposeLRP(namespace, serviceName string) {
+	Expect(fixture.Clientset.CoreV1().Services(namespace).Delete(context.Background(), serviceName, metav1.DeleteOptions{})).To(Succeed())
+}
+
 func verifyCrashRequest(requestMatcher wiremock.RequestMatcher, exitStatus int) {
 	body, err := fixture.Wiremock.GetRequestBody(requestMatcher)
 	Expect(err).NotTo(HaveOccurred())
@@ -153,5 +191,5 @@ func verifyCrashRequest(requestMatcher wiremock.RequestMatcher, exitStatus int) 
 	Expect(err).NotTo(HaveOccurred())
 
 	Expect(request.ExitStatus).To(Equal(exitStatus))
-	Expect(request.CrashCount).To(BeNumerically(">=", 1))
+	Expect(request.CrashCount).To(Equal(1))
 }
