a928dd14f2b959ff855ec3291714831c48837219
Georgi Sabev <georgethebeatle@gmail.com>
2020-12-04 10:38:13 +0000

    Add a script to patch the cloud_controller_ng
    It builds the cloud_controller_ng with pack and does a kubectl patch of
    the cf-api-server deployment
    
    [#175417248]

---
+++ scripts/patch-cc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -eu
+
+CCNG_DIR="$HOME/workspace/capi-release/src/cloud_controller_ng"
+TAG=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
+CCNG_IMAGE="eirini/dev-ccng:$TAG"
+
+build_ccng_image() {
+  pushd "$CCNG_DIR"
+  {
+    pack build --builder "paketobuildpacks/builder:full" "$CCNG_IMAGE"
+  }
+  popd
+}
+
+load-into-kind() {
+  local current_context kind_cluster_name
+  # assume we are pointed to a kind cluster
+  current_context=$(yq r "$HOME/.kube/config" 'current-context')
+  # strip the 'kind-' prefix that kind puts in the context name
+  kind_cluster_name=${current_context#"kind-"}
+
+  kind load docker-image --name "$kind_cluster_name" "$CCNG_IMAGE"
+}
+
+patch-cf-api-server() {
+  local patch_file
+
+  patch_file=$(mktemp)
+  trap "rm $patch_file" EXIT
+
+  cat <<EOF >>"$patch_file"
+spec:
+  template:
+    spec:
+      containers:
+      - name: cf-api-server
+        image: "$CCNG_IMAGE"
+        imagePullPolicy: IfNotPresent
+EOF
+
+  kubectl -n cf-system patch deployment cf-api-server --patch "$(cat "$patch_file")"
+}
+
+main() {
+  build_ccng_image
+  load-into-kind
+  patch-cf-api-server
+}
+
+main
