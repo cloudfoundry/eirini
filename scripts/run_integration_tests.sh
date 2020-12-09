fee928b84f0682d76ab6bf48ea5b5a8534b78a1b
Georgi Sabev <georgethebeatle@gmail.com>
2020-12-09 15:25:32 +0000

    Revert run_integration_tests script
    Previous changes broke the focus functionality

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 readonly BASEDIR="$(cd "$(dirname "$0")"/.. && pwd)"
-
 export GO111MODULE=on
-export EIRINI_BINS_PATH=${EIRINI_BINS_PATH:-$(mktemp -d)}
-
 if [ -z ${EIRINIUSER_PASSWORD+x} ]; then
   EIRINIUSER_PASSWORD="$(pass eirini/docker-hub)"
 fi
@@ -16,33 +13,14 @@ if [[ "${NODES:-}" != "" ]]; then
   nodes="-nodes $NODES"
 fi
 
-build() {
-  local bin dir
-
-  for dir in $BASEDIR/cmd/*/; do
-    bin=$(basename $dir)
-    CGO_ENABLED=0 GOOS=linux go build -mod vendor -trimpath -installsuffix cgo -o "$EIRINI_BINS_PATH/$bin" "$dir" &
-  done
-
-  ginkgo build -mod=vendor -r &
-
-  wait
-}
-
-run() {
-  trap "rm -rf $EIRINI_BINS_PATH" EXIT
-  ginkgo -mod=vendor -r -p $nodes -keepGoing -randomizeAllSpecs -randomizeSuites -timeout=20m "$@" $(find . -name "*.test")
-}
-
 main() {
-  action=${1:-"run"}
-  if [ $# -gt 0 ]; then
-    shift
-  fi
+  export EIRINI_BINS_PATH
+  EIRINI_BINS_PATH=$(mktemp -d)
+  trap "rm -rf $EIRINI_BINS_PATH" EXIT
 
   pushd "$BASEDIR"/tests/integration >/dev/null || exit 1
   {
-    ${action}
+    ginkgo -mod=vendor -p $nodes -r -keepGoing -tags=integration -randomizeAllSpecs -randomizeSuites -timeout=20m "$@"
   }
   popd >/dev/null || exit 1
 }
