77cc059d6eb1f69a6d573e097a49909a604784e6
Danail Branekov <danailster@gmail.com>
2021-02-17 11:01:28 +0000

    Use cf-for-k8s value overrides when patching
    [#176842452]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- scripts/patch-me-if-you-can.sh
+++ scripts/patch-me-if-you-can.sh
@@ -207,7 +207,7 @@ patch_cf_for_k8s() {
   render_dir="$(mktemp -d)"
   trap "rm -rf $render_dir" EXIT
 
-  "$EIRINI_RELEASE_BASEDIR/scripts/render-templates.sh" cf-system "$render_dir"
+  "$EIRINI_RELEASE_BASEDIR/scripts/render-templates.sh" cf-system "$render_dir" --values "$EIRINI_RELEASE_BASEDIR/scripts/assets/cf-for-k8s-value-overrides.yml"
 
   rm -rf "$CF4K8S_DIR/build/eirini/_vendir/eirini"
   mv "${render_dir}/templates" "$CF4K8S_DIR/build/eirini/_vendir/eirini"
