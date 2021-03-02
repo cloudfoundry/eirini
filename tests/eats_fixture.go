fa06bdffe76c04d96e5ca849b417c9334dc92c85
Georgi Sabev <georgethebeatle@gmail.com>
2021-03-02 15:38:03 +0000

    Move CopyRolesAndBindings function in integration package
    [#176075325]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- tests/eats_fixture.go
+++ tests/eats_fixture.go
@@ -19,8 +19,10 @@ import (
 	// nolint:golint,stylecheck,revive
 	. "github.com/onsi/gomega"
 	"gopkg.in/yaml.v2"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 )
 
 type EATSFixture struct {
@@ -159,4 +161,38 @@ func NewWiremock() *wiremock.Wiremock {
 	RetryResolveHost(wireMockHost, "Is wiremock running in the cluster?")
 
 	return wiremock.New(wireMockHost)
+}
+
+func CopyRolesAndBindings(namespace string, clientset kubernetes.Interface) {
+	from := GetEiriniWorkloadsNamespace()
+
+	roleList, err := clientset.RbacV1().Roles(from).List(context.Background(), metav1.ListOptions{})
+	Expect(err).NotTo(HaveOccurred())
+
+	for _, role := range roleList.Items {
+		newRole := new(rbacv1.Role)
+		newRole.Namespace = namespace
+		newRole.Name = role.Name
+		newRole.Rules = role.Rules
+		_, err = clientset.RbacV1().Roles(namespace).Create(context.Background(), newRole, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	bindingList, err := clientset.RbacV1().RoleBindings(from).List(context.Background(), metav1.ListOptions{})
+	Expect(err).NotTo(HaveOccurred())
+
+	for _, binding := range bindingList.Items {
+		newBinding := new(rbacv1.RoleBinding)
+		newBinding.Namespace = namespace
+		newBinding.Name = binding.Name
+		newBinding.Subjects = binding.Subjects
+
+		if binding.Name == "eirini-workloads-app-rolebinding" {
+			newBinding.Subjects[0].Namespace = namespace
+		}
+
+		newBinding.RoleRef = binding.RoleRef
+		_, err := clientset.RbacV1().RoleBindings(namespace).Create(context.Background(), newBinding, metav1.CreateOptions{})
+		Expect(err).NotTo(HaveOccurred())
+	}
 }
