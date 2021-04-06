a7a1fb62746b889c7d79837908f46b231ffb2339
Danail Branekov <danailster@gmail.com>
2021-04-06 11:14:59 +0000

    Register webhook only once in instance injector test
    This fixes test flakiness. Apparently multiple registered hooks not
    limited to different namespaces can interfere.
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tests/integration/instance-index-injector/instance_index_injector_test.go
+++ tests/integration/instance-index-injector/instance_index_injector_test.go
@@ -2,101 +2,19 @@ package instance_index_injector_test
 
 import (
 	"context"
-	"crypto/tls"
-	"fmt"
-	"net/http"
-	"os"
 
 	"code.cloudfoundry.org/eirini"
 	"code.cloudfoundry.org/eirini/k8s/stset"
-	"code.cloudfoundry.org/eirini/tests"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
-	arv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("InstanceIndexInjector", func() {
-	var (
-		config         *eirini.InstanceIndexEnvInjectorConfig
-		configFilePath string
-		hookSession    *gexec.Session
-		pod            *corev1.Pod
-		fingerprint    string
-		certDir        string
-	)
+	var pod *corev1.Pod
 
 	BeforeEach(func() {
-		port := int32(tests.GetTelepresencePort())
-		telepresenceService := tests.GetTelepresenceServiceName()
-		telepresenceDomain := fmt.Sprintf("%s.default.svc", telepresenceService)
-		fingerprint = "instance-id-" + tests.GenerateGUID()[:8]
-		var caBundle []byte
-		certDir, caBundle = tests.GenerateKeyPairDir("tls", telepresenceDomain)
-		sideEffects := arv1.SideEffectClassNone
-		scope := arv1.NamespacedScope
-
-		_, err := fixture.Clientset.AdmissionregistrationV1().MutatingWebhookConfigurations().Create(context.Background(),
-			&arv1.MutatingWebhookConfiguration{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: fingerprint + "-mutating-hook",
-				},
-				Webhooks: []arv1.MutatingWebhook{
-					{
-						Name: fingerprint + "-mutating-webhook.cloudfoundry.org",
-						ClientConfig: arv1.WebhookClientConfig{
-							Service: &arv1.ServiceReference{
-								Namespace: "default",
-								Name:      telepresenceService,
-								Port:      &port,
-							},
-							CABundle: caBundle,
-						},
-						SideEffects:             &sideEffects,
-						AdmissionReviewVersions: []string{"v1beta1"},
-						Rules: []arv1.RuleWithOperations{
-							{
-								Operations: []arv1.OperationType{arv1.Create},
-								Rule: arv1.Rule{
-									APIGroups:   []string{""},
-									APIVersions: []string{"v1"},
-									Resources:   []string{"pods"},
-									Scope:       &scope,
-								},
-							},
-						},
-					},
-				},
-			}, metav1.CreateOptions{})
-		Expect(err).NotTo(HaveOccurred())
-
-		config = &eirini.InstanceIndexEnvInjectorConfig{
-			Port: port,
-			KubeConfig: eirini.KubeConfig{
-				ConfigPath: fixture.KubeConfigPath,
-			},
-		}
-		env := fmt.Sprintf("%s=%s", eirini.EnvInstanceEnvInjectorCertDir, certDir)
-		hookSession, configFilePath = eiriniBins.InstanceIndexEnvInjector.Run(config, env)
-
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client := &http.Client{Transport: tr}
-		Eventually(func() (int, error) {
-			resp, err := client.Get(fmt.Sprintf("https://%s:%d/", telepresenceDomain, port))
-			if err != nil {
-				printMessage(fmt.Sprintf("failed to call telepresence: %s", err.Error()))
-
-				return 0, err
-			}
-			resp.Body.Close()
-
-			return resp.StatusCode, nil
-		}, "2m", "500ms").Should(Equal(http.StatusOK))
-
 		pod = &corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "app-name-0",
@@ -117,19 +35,6 @@ var _ = Describe("InstanceIndexInjector", func() {
 				},
 			},
 		}
-	})
-
-	AfterEach(func() {
-		if configFilePath != "" {
-			Expect(os.Remove(configFilePath)).To(Succeed())
-		}
-		if hookSession != nil {
-			Eventually(hookSession.Kill()).Should(gexec.Exit())
-		}
-		err := fixture.Clientset.AdmissionregistrationV1().MutatingWebhookConfigurations().Delete(context.Background(), fingerprint+"-mutating-hook", metav1.DeleteOptions{})
-		Expect(err).NotTo(HaveOccurred())
-
-		Expect(os.RemoveAll(certDir)).To(Succeed())
 	})
 
 	JustBeforeEach(func() {
@@ -164,8 +69,3 @@ var _ = Describe("InstanceIndexInjector", func() {
 		Expect(getCFInstanceIndex(pod, "not-opi")).To(Equal(""))
 	})
 })
-
-func printMessage(str string) {
-	_, err := GinkgoWriter.Write([]byte(str))
-	Expect(err).NotTo(HaveOccurred())
-}
