8d217a1c31c58a78224fbf1a745c815b731be171
Mario Nitchev <marionitchev@gmail.com>
2021-04-14 08:39:04 +0000

    Add missing crd integration tests
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tests/integration/eirini-controller/app_test.go
+++ tests/integration/eirini-controller/app_test.go
@@ -12,6 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -55,16 +56,9 @@ var _ = Describe("App", func() {
 	})
 
 	Describe("desiring an app", func() {
-		var st *appsv1.StatefulSet
+		var createErr error
 
-		JustBeforeEach(func() {
-			var err error
-			lrp, err = fixture.EiriniClientset.
-				EiriniV1().
-				LRPs(fixture.Namespace).
-				Create(context.Background(), lrp, metav1.CreateOptions{})
-			Expect(err).NotTo(HaveOccurred())
-
+		getRunningStatefulset := func() *appsv1.StatefulSet {
 			Eventually(func() *appsv1.StatefulSet {
 				return integration.GetStatefulSet(fixture.Clientset, fixture.Namespace, lrpGUID, lrpVersion)
 			}).ShouldNot(BeNil())
@@ -73,11 +67,35 @@ var _ = Describe("App", func() {
 				return getPodReadiness(lrpGUID, lrpVersion)
 			}).Should(BeTrue(), "LRP Pod not ready")
 
-			st = integration.GetStatefulSet(fixture.Clientset, fixture.Namespace, lrpGUID, lrpVersion)
+			return integration.GetStatefulSet(fixture.Clientset, fixture.Namespace, lrpGUID, lrpVersion)
+		}
+
+		JustBeforeEach(func() {
+			lrp, createErr = fixture.EiriniClientset.
+				EiriniV1().
+				LRPs(fixture.Namespace).
+				Create(context.Background(), lrp, metav1.CreateOptions{})
+		})
+
+		It("creates a statefulset for the app", func() {
+			Expect(createErr).NotTo(HaveOccurred())
+			st := getRunningStatefulset()
+			Expect(st.Labels[stset.LabelGUID]).To(Equal(lrp.Spec.GUID))
 		})
 
 		It("sets the runAsNonRoot in the PodSecurityContext", func() {
+			st := getRunningStatefulset()
 			Expect(st.Spec.Template.Spec.SecurityContext.RunAsNonRoot).To(PointTo(BeTrue()))
+		})
+
+		When("DiskMB is not set", func() {
+			BeforeEach(func() {
+				lrp.Spec.DiskMB = 0
+			})
+
+			It("errors", func() {
+				Expect(createErr).To(MatchError(ContainSubstring("Invalid value")))
+			})
 		})
 
 		When("AllowRunImageAsRoot is true", func() {
@@ -86,7 +104,67 @@ var _ = Describe("App", func() {
 			})
 
 			It("doesn't set `runAsNonRoot` in the PodSecurityContext", func() {
+				st := getRunningStatefulset()
 				Expect(st.Spec.Template.Spec.SecurityContext.RunAsNonRoot).To(BeNil())
+			})
+		})
+
+		Describe("automounting serviceacccount token", func() {
+			const serviceAccountTokenMountPath = "/var/run/secrets/kubernetes.io/serviceaccount"
+			var podMountPaths []string
+
+			JustBeforeEach(func() {
+				Eventually(func() ([]corev1.Pod, error) {
+					pods, err := fixture.Clientset.CoreV1().Pods(fixture.Namespace).List(context.Background(), metav1.ListOptions{})
+					if err != nil {
+						return nil, err
+					}
+
+					return pods.Items, nil
+				}).ShouldNot(BeEmpty())
+
+				pods, err := fixture.Clientset.CoreV1().Pods(fixture.Namespace).List(context.Background(), metav1.ListOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(pods.Items).To(HaveLen(1))
+
+				podMountPaths = []string{}
+				for _, podMount := range pods.Items[0].Spec.Containers[0].VolumeMounts {
+					podMountPaths = append(podMountPaths, podMount.MountPath)
+				}
+			})
+
+			It("does not mount the service account token", func() {
+				Expect(podMountPaths).NotTo(ContainElement(serviceAccountTokenMountPath))
+			})
+
+			When("unsafe_allow_automount_service_account_token is set", func() {
+				BeforeEach(func() {
+					config.UnsafeAllowAutomountServiceAccountToken = true
+				})
+
+				It("mounts the service account token (because this is how K8S works by default)", func() {
+					Expect(podMountPaths).To(ContainElement(serviceAccountTokenMountPath))
+				})
+
+				When("the app service account has its automountServiceAccountToken set to false", func() {
+					updateServiceaccount := func() error {
+						appServiceAccount, err := fixture.Clientset.CoreV1().ServiceAccounts(fixture.Namespace).Get(context.Background(), tests.GetApplicationServiceAccount(), metav1.GetOptions{})
+						Expect(err).NotTo(HaveOccurred())
+						automountServiceAccountToken := false
+						appServiceAccount.AutomountServiceAccountToken = &automountServiceAccountToken
+						_, err = fixture.Clientset.CoreV1().ServiceAccounts(fixture.Namespace).Update(context.Background(), appServiceAccount, metav1.UpdateOptions{})
+
+						return err
+					}
+
+					BeforeEach(func() {
+						Eventually(updateServiceaccount, "5s").Should(Succeed())
+					})
+
+					It("does not mount the service account token", func() {
+						Expect(podMountPaths).NotTo(ContainElement(serviceAccountTokenMountPath))
+					})
+				})
 			})
 		})
 	})
